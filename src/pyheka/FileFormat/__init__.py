23cef0293bd763c8c3939ef30bdb7e459dfb05c0
Igor Delvendahl <igordelvendahl@gmail.com>
2026-09-08 12:59:33 +0200

    :bug: fix import issue

--- src/pyheka/FileFormat/__init__.py
+++ src/pyheka/FileFormat/__init__.py
@@ -1,2 +1,2 @@
-from .FileFormat_common import Data
-from . import FileFormat_v9, FileFormat_v1000, FileFormat_v2000
+from pyheka.FileFormat.FileFormat_common import Data
+from pyheka.FileFormat import FileFormat_v9, FileFormat_v1000, FileFormat_v2000
--- src/pyheka/__init__.py
+++ src/pyheka/__init__.py
@@ -1,3 +1,3 @@
-from .pyheka import Bundle
+from pyheka.pyheka import Bundle
 
 __all__ = ["Bundle"]
